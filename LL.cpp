a76dfbb9fca7cfac4dd27bc932436276effe0878
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-04-26 18:39:30 -0400

    version 1.0

--- LL.cpp
+++ LL.cpp
@@ -12,117 +12,167 @@ using namespace std;
 template <class T>
 class Node {
 public:
-  Node( const T& data ); //Node Constructor - Assume each quan
-  pair<T,int>& GetData(); //Node Data Getter
-  void SetData( const pair<T,int>& ); //Node Data Setter
-  Node<T>* GetNext(); //Node m_next getter
-  void SetNext( Node<T>* next ); //Node m_next setter
+    Node(const T& data); //Node Constructor - Assume each quan
+    pair<T, int>& GetData(); //Node Data Getter
+    void SetData(const pair<T, int>&); //Node Data Setter
+    Node<T>* GetNext(); //Node m_next getter
+    void SetNext(Node<T>* next); //Node m_next setter
 private:
-  pair <T,int> m_data; //Holds templated data
-  Node<T>* m_next; //Pointer to next node
+    pair <T, int> m_data; //Holds templated data
+    Node<T>* m_next; //Pointer to next node
 };
 
 template <class T>
-Node<T>::Node( const T& data ) {
-  m_data = make_pair(data,1);
-  m_next = nullptr;
+Node<T>::Node(const T& data) {
+    m_data = make_pair(data, 1);
+    m_next = nullptr;
 }
 
 template <class T>
-pair<T,int>& Node<T>::GetData() {
-   return m_data;
+pair<T, int>& Node<T>::GetData() {
+    return m_data;
 }
 
 template <class T>
-void Node<T>::SetData( const pair<T,int>& data ) {
-   m_data = data;
+void Node<T>::SetData(const pair<T, int>& data) {
+    m_data = data;
 }
 
 template <class T>
 Node<T>* Node<T>::GetNext() {
-   return m_next;
+    return m_next;
 }
 
 template <class T>
-void Node<T>::SetNext( Node<T>* next ) {
-   m_next = next;
+void Node<T>::SetNext(Node<T>* next) {
+    m_next = next;
 }
 
 //Templated Linked List class
 template <class T>
 class LL {
- public:
-  // Name: LL() (Linked List) - Default Constructor
-  // Desc: Used to build a new linked list
-  // Preconditions: None
-  // Postconditions: Creates a new linked list where m_head points to nullptr
-  LL();
-  // Name: ~LL() - Destructor
-  // Desc: Used to destruct a LL
-  // Preconditions: There is an existing LL with at least one node
-  // Postconditions: A LL is deallocated (including dynamically allocated nodes)
-  //                 to have no memory leaks!
- ~LL();
-  // Name: LL (Copy Constructor)
-  // Desc: Creates a copy of existing LL
-  //       Requires a LL - REQUIRED to be implemented even if not used
-  // Preconditions: Source LL exists
-  // Postconditions: Copy of source LL
-  LL(const LL&);
-  // Name: operator= (Overloaded Assignment Operator)
-  // Desc: Makes two LL of the same type have identical number of nodes and values
-  // Preconditions: Requires two linked lists of the same templated type
-  //                REQUIRED to be implemented even if not used
-  // Postconditions: Two idenetical LL
-  LL<T>& operator= (const LL&);
-  // Name: Find
-  // Desc: Iterates through LL and returns node if data found
-  // Preconditions: LL Populated
-  // Postconditions: Returns nullptr if not found OR Node pointer if found
-  Node<T>* Find(const T& data);
-  // Name: Insert
-  // Desc: Either inserts a node to the linked list OR increments frequency of first
-  //       Takes in data. If "first" (of pair) NOT in list, adds node
-  //       If "first" (of pair) is already in list, increments quantity
-  //       Inserts "first" in order with no duplicates
-  // Preconditions: Requires a LL.
-  // Postconditions: Node inserted in LL based on first value (or quantity incremented)
-  void Insert(const T&);
-  // Name: RemoveAt
-  // Desc: Removes a node at a particular position based on data passed (matches first)
-  // Preconditions: LL with at least one node. 
-  // Postconditions: Removes first node with passed value (in first)
-  void RemoveAt(const T&);
-  // Name: Display
-  // Desc: Display all nodes in linked list
-  // Preconditions: Outputs the LL
-  // Postconditions: Displays the pair in each node of LL 
-  void Display();
-  // Name: GetSize
-  // Desc: Returns the size of the LL
-  // Preconditions: Requires a LL
-  // Postconditions: Returns m_size
-  int GetSize();
-  // Name: operator<< (Overloaded << operator)
-  // Desc: Returns the ostream of the data in each node
-  // Preconditions: Requires a LL
-  // Postconditions: Returns an ostream with the data from each node on different line
-  template <class U>
-  friend ostream& operator<<(ostream& output, const LL<U>&);
-  // Name: Overloaded [] operator
-  // Desc: When passed an integer, returns the data at that location
-  // Precondition: Existing LL
-  // Postcondition: Returns pair from LL using []
-  pair<T,int>& operator[] (int x);//Overloaded [] operator to pull data from LL
+public:
+    // Name: LL() (Linked List) - Default Constructor
+    // Desc: Used to build a new linked list
+    // Preconditions: None
+    // Postconditions: Creates a new linked list where m_head points to nullptr
+    LL();
+
+    // Name: ~LL() - Destructor
+    // Desc: Used to destruct a LL
+    // Preconditions: There is an existing LL with at least one node
+    // Postconditions: A LL is deallocated (including dynamically allocated nodes)
+    //                 to have no memory leaks!
+    ~LL();
+
+    // Name: LL (Copy Constructor)
+    // Desc: Creates a copy of existing LL
+    //       Requires a LL - REQUIRED to be implemented even if not used
+    // Preconditions: Source LL exists
+    // Postconditions: Copy of source LL
+    LL(const LL&);
+
+    // Name: operator= (Overloaded Assignment Operator)
+    // Desc: Makes two LL of the same type have identical number of nodes and values
+    // Preconditions: Requires two linked lists of the same templated type
+    //                REQUIRED to be implemented even if not used
+    // Postconditions: Two idenetical LL
+    LL<T>& operator= (const LL&);
+
+    // Name: Find
+    // Desc: Iterates through LL and returns node if data found
+    // Preconditions: LL Populated
+    // Postconditions: Returns nullptr if not found OR Node pointer if found
+    Node<T>* Find(const T& data);
+
+    // Name: Insert
+    // Desc: Either inserts a node to the linked list OR increments frequency of first
+    //       Takes in data. If "first" (of pair) NOT in list, adds node
+    //       If "first" (of pair) is already in list, increments quantity
+    //       Inserts "first" in order with no duplicates
+    // Preconditions: Requires a LL.
+    // Postconditions: Node inserted in LL based on first value (or quantity incremented)
+    void Insert(const T&);
+
+    // Name: RemoveAt
+    // Desc: Removes a node at a particular position based on data passed (matches first)
+    // Preconditions: LL with at least one node. 
+    // Postconditions: Removes first node with passed value (in first)
+    void RemoveAt(const T&);
+
+    // Name: Display
+    // Desc: Display all nodes in linked list
+    // Preconditions: Outputs the LL
+    // Postconditions: Displays the pair in each node of LL 
+    void Display();
+
+    // Name: GetSize
+    // Desc: Returns the size of the LL
+    // Preconditions: Requires a LL
+    // Postconditions: Returns m_size
+    int GetSize();
+
+    // Name: operator<< (Overloaded << operator)
+    // Desc: Returns the ostream of the data in each node
+    // Preconditions: Requires a LL
+    // Postconditions: Returns an ostream with the data from each node on different line
+    template <class U>
+    friend ostream& operator<<(ostream& output, const LL<U>&);
+
+    // Name: Overloaded [] operator
+    // Desc: When passed an integer, returns the data at that location
+    // Precondition: Existing LL
+    // Postcondition: Returns pair from LL using []
+    pair<T, int>& operator[] (int x);//Overloaded [] operator to pull data from LL
+
 private:
-  Node <T> *m_head; //Node pointer for the head
-  int m_size; //Number of nodes in queue
+    Node <T>* m_head; //Node pointer for the head
+    int m_size; //Number of nodes in queue
 };
 //*****************************************************************
 //Implement LL here
 
+template<class T>
+LL<T>::LL() {}
 
+template<class T>
+LL<T>::~LL() {}
 
+template<class T>
+LL<T>::LL(const LL&) {}
+
+template<class T>
+LL<T>& LL<T>::operator=(const LL&) {}
+
+template<class T>
+Node<T>* LL<T>::Find(const T& data){}
+
+template<class T>
+void LL<T>::Insert(const T& newNode) {
+
+    // If the size of the list is zero
+    // set the node to m_head 
+    
+    // Otherwise, see if you can find the node in the list
+
+    // If it is on the list, then increment the quantity 
+    // otherwise, add the new node in the list in alphabetical order
+}
+
+template<class T>
+void LL<T>::RemoveAt(const T&) {}
+
+template<class T>
+void LL<T>::Display() {}
+
+template<class T>
+int LL<T>::GetSize() {}
+
+template<class U>
+ostream& operator<<(ostream& output, const LL<U>&) {}
+
+template<class T>
+pair<T, int>& LL<T>::operator[] (int x) {}
 
 
 
@@ -170,7 +220,7 @@ int main () {
   newLL1->Display();
   cout << "*******Copy*********" << endl;
   newLL2->Display();
-    
+
   //Test Overloaded Assignment Operator
   LL <string>* newLL3 = new LL<string>();
   newLL3 = newLL1;
