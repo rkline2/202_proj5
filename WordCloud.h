9c577d7d46be5ed5e1a9e78ea6dfa76ce5028ba0
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-05-05 16:48:42 -0400

    version 1.1 added consts only

--- WordCloud.h
+++ WordCloud.h
@@ -21,7 +21,7 @@ const char SEPARATOR = ':';
 const char NEWLINE = '\n';
 
 const string SIN_YES = "y";
-const string FULL_YES = "Y";
+const string FULL_YES = "yes";
 //Common words to be removed from output
 const vector<string> EXCLUDE_LIST{ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "can't", "cannot", "com", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "else", "ever", "few", "for", "from", "further", "get", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "http", "i", "i'd", "i'll", "i'm", "i've", "I", "I'd", "I'll", "I'm", "I've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "just", "k", "let's", "like", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "r", "same", "shall", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "www", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" };
 
@@ -79,6 +79,9 @@ public:
     // Preconditions: None (filename is available)
     // Postconditions: None
     void Start();
+
+    // REMOVE WHEN FINISHED!!
+    void test(string);
 private:
     string m_fileName; //Name of the input file
     LL<string>* m_cloud; //LL to store all data
